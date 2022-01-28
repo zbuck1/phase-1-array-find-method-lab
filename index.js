04587854ce3a45e6f62fe9405b59e0901fd74fdb
Zoe <buckzoe5@gmail.com>
2022-01-28 13:37:45 -0500

    commit tree 2c70d08b7644782c77eba62b63740a18b4a99adc

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin() {
+    const winningGame = array.find(game => game.result === '2015');
+    return winningGame.year;
+}
+superbowlWin = (record) => {
+    if (record.result === 'W') {
+        return record.year
+    }
+}
